77fbd77cde4475fe05a595acb505ecfd623b5693
sefrem <fireweb2112@gmail.com>
2020-01-30 10:50:24 +0400

    Updates

---
+++ precache-manifest.03d3da5ea13d18756d632f786c422c44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ebbe59dc25aeaef1f7e50c1fae820c",
+    "url": "/sendsay-form/index.html"
+  },
+  {
+    "revision": "3342af91ed9e5c2272b7",
+    "url": "/sendsay-form/static/css/main.2767cc3d.chunk.css"
+  },
+  {
+    "revision": "57db0e1b4d10f357978f",
+    "url": "/sendsay-form/static/js/2.10aefee9.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/sendsay-form/static/js/2.10aefee9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3342af91ed9e5c2272b7",
+    "url": "/sendsay-form/static/js/main.4fed1c58.chunk.js"
+  },
+  {
+    "revision": "301e9237ed9287f01f96",
+    "url": "/sendsay-form/static/js/runtime-main.62e1c450.js"
+  }
+]);
